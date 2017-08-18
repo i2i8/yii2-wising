33c940da73a8537560f8c85fc1ec6c79851710cc
i2i8 <i2i8@qq.com>
2017-08-18 15:30:42 +0800

    init commit

---
+++ components/MessageCrypt.php
@@ -0,0 +1,20 @@
+<?php
+namespace i2i8\wising\components;
+
+require_once 'messageCrypt/wxBizMsgCrypt.php';
+
+/**
+ * 消息加密类
+ * @package xfstudio\wechat
+ */
+class MessageCrypt extends \WXBizMsgCrypt
+{
+    /**
+     * Returns the fully qualified name of this class.
+     * @return string the fully qualified name of this class.
+     */
+    public static function className()
+    {
+        return get_called_class();
+    }
+}
---
+++ components/WechatComponent.php
@@ -0,0 +1,26 @@
+<?php
+namespace i2i8\wising\components;
+
+use yii\base\Object;
+
+/**
+ * 微信
+ * @package xfstudio\wechat\components
+ */
+class WechatComponent extends Object
+{
+    /**
+     * @var BaseWechat
+     */
+    protected $wechat;
+
+    /**
+     * @param BaseWechat $wechat
+     * @param array $config
+     */
+    public function __construct(BaseWechat $wechat, $config = [])
+    {
+        $this->wechat = $wechat;
+        parent::__construct($config);
+    }
+}
